1f0d2985e8767b567f168d647cfe804306ed0e89
Abhishek Upperwal <a.upperwal@gmail.com>
2020-03-04 21:27:04 +0530

    bump ver number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pravah",
-    version="0.1.1",
+    version="0.1.2",
     author="Abhishek Upperwal",
     author_email="mesh@soket.in",
     description="Data access layer",
